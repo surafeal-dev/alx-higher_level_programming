0bd4d728074de7e7f7f0ea291a5bddc4f2e8972c
surafeal-dev <98810077+surafeal-dev@users.noreply.github.com>
2022-08-11 21:59:43 +0300

    Create 16-no_link.sql

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,14 @@
+-- list all coincidences--
+SELECT
+    score,
+    name
+FROM second_table WHERE name IS NOT NULL
+ORDER BY score DESC;
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+D
