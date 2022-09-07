70c07c72464036ac789eb00c8935ace5fee6d4f0
surafeal-dev <98810077+surafeal-dev@users.noreply.github.com>
2022-09-07 17:06:14 +0300

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
-const dictInput = require('./101-data').dict;
-const outDict = {};
-
-for (const key in dictInput) {
-  const ocurr = dictInput[key];
-  outDict[ocurr] = [];
-  for (const keys in dictInput) {
-    if (dictInput[keys] === ocurr) {
-      outDict[ocurr].push(keys);
-    }
-  }
+const dic = require('./101-data.js').dict;
+const newDic = {};
+for (const key in dic) {
+  if (newDic[dic[key]]) newDic[dic[key]].push(key);
+  else newDic[dic[key]] = [key];
 }
-console.log(outDict);
+console.log(newDic);
